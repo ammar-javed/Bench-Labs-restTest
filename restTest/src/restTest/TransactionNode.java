193ddb6e9005713e8360471bc4a84d15613c34a7
Ammar <ammarj965@gmail.com>
2016-05-02 14:09:02 -0400

    Added new functionality for parsing Company names in TransactionNodes; attempt to clean up company name on financial reports for users.

--- restTest/src/restTest/TransactionNode.java
+++ restTest/src/restTest/TransactionNode.java
@@ -5,6 +5,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Ammar Javed
@@ -20,7 +22,9 @@ class TransactionNode {
 	
 	double amount;
 	
-	String company;
+	String company; 
+	
+	String formattedCompany;
 	
 	TransactionNode next;
 	
@@ -42,5 +46,40 @@ class TransactionNode {
 		this.company = c;
 		
 		this.next = null;
+		
+		this.formattedCompany = validateCompanyName(c);
+	}
+	
+	/**
+	 * Company original Company name from transaction against a regular expression.
+	 * If it matches, we may have a potentially readable name with extra garbage values removed.
+	 * 
+	 * If it doesn't match, we will return the original company name.
+	 * 
+	 * @param c Raw company name
+	 * @return extracted Company name, or old company name if none were found
+	 */
+	private String validateCompanyName(String c) {
+		// Explanation of Regular expression can be found here: https://regex101.com/r/bA2tD7/1
+		String pattern = "^[.@&]?[a-zA-Z0-9 ]+[ '!.@&()]?[ a-zA-Z0-9!()]+";
+		
+		Pattern p = Pattern.compile(pattern);
+		
+		Matcher m = p.matcher(c);
+		
+		if (m.find()){
+			return m.group(0);
+		}
+		
+		return c;
+	}
+	
+	public String getFormattedCompanyName(){
+		
+		if (this.formattedCompany.length() > 15){
+			return this.formattedCompany.substring(0, 13) + "...";
+		}
+		
+		return this.formattedCompany;
 	}
 }
--- restTest/src/restTest/restTest.java
+++ restTest/src/restTest/restTest.java
@@ -88,7 +88,6 @@ public class restTest {
 				calculateAndPrintBalance();
 
 			} catch (JSONException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}			
 		}
@@ -127,270 +126,6 @@ public class restTest {
 			}
 		}
 	}
-	
-	/**
-	 * Format and print appropriate header for Daily balance report
-	 */
-	private static void printDailyBalanceHeader() {
-		if (verbose || daily || dailyVerbose) {
-			System.out.println("\n\n");
-			printAndFormatColumns("", "Daily Balance Report", "");
-			printAndFormatColumns("", "--------------------", "");
-			System.out.println("\n\n");
-		}
-		
-		if (!verbose && daily) {
-			printAndFormatColumns("Daily Balance", "Payments", "Expenses", "Date");
-			printAndFormatColumns("-------------", "--------", "--------", "----");
-		} else if (verbose || dailyVerbose) {
-			printAndFormatColumns("Company", "Daily Balance", "Payments", "Expenses", "Date");
-			printAndFormatColumns("-------", "-------------", "--------", "--------", "----");
-		}
-	}
-	
-	private static void printSummaryHeader() {
-		
-		printAndFormatColumns("", "Summary Report", "");
-		printAndFormatColumns("", "--------------", "");
-		System.out.println("\n");
-		printAndFormatColumns("Final Balance", "Total Payments", "Total Expenses");
-		printAndFormatColumns("-------------", "--------------", "--------------");
-		
-	}
-
-	/**
-	 * Calculates daily and total balance.
-	 * It brings the daily balance only if the flag is set, otherwise 
-	 * only calculates and prints the final summary
-	 */
-	private static void calculateAndPrintBalance(){		
-		// Sort dates stored in transactionsMap
-		ArrayList<String> dates = new ArrayList<String>(transactionsMap.keySet());
-		java.util.Collections.sort(dates);
-		ArrayList<TransactionNode> transactions;
-		
-		// Nifty decimal formatting pattern found online!
-		DecimalFormat currencyFormat = new DecimalFormat("$#,##0.00;-$#,##0.00");
-		
-		// Totals
-		double totalBalance = 0;
-		double totalExpenses = 0;
-		double totalPayments = 0;
-		
-		// Daily figures
-		double dailyBalance;
-		double dailyExpenses;
-		double dailyPayments;
-		
-		printDailyBalanceHeader();
-		
-		for (String date : dates) {
-			transactions = transactionsMap.get(date);
-			
-			dailyBalance = 0;
-			dailyExpenses = 0;
-			dailyPayments = 0;
-			
-			for (TransactionNode trans : transactions) {
-				
-				dailyBalance += trans.amount;
-				
-				if (trans.amount < 0) {
-					
-					if (verbose || dailyVerbose)
-						printAndFormatColumns(trans.company.substring(0, 10),
-											  "",
-											  "",
-											  currencyFormat.format(trans.amount),
-											  date);
-					
-					dailyExpenses += trans.amount;
-				} else {
-					if (verbose || dailyVerbose)
-						printAndFormatColumns(trans.company.substring(0, 10),
-											  "",
-											  currencyFormat.format(trans.amount),
-											  "",
-											  date);
-					
-					dailyPayments += trans.amount;
-				}
-			}
-			
-			totalBalance += dailyBalance;
-			totalExpenses += dailyExpenses;
-			totalPayments += dailyPayments;
-			
-			if (!verbose && daily){
-				
-				printAndFormatColumns(currencyFormat.format(totalBalance),
-									  currencyFormat.format(dailyPayments),
-								   	  currencyFormat.format(dailyExpenses),
-								   	  date);
-				System.out.println("");
-				
-			} else if (verbose || dailyVerbose) {
-				
-				System.out.println("");
-				printAndFormatColumns("Total", 
-									  currencyFormat.format(dailyBalance),
-									  currencyFormat.format(dailyPayments),
-									  currencyFormat.format(dailyExpenses),
-									  "");
-				System.out.println("");
-				
-			}
-		}
-		
-		printSummaryHeader();
-		
-		printAndFormatColumns(currencyFormat.format(totalBalance),
-							  currencyFormat.format(totalPayments),
-							  currencyFormat.format(totalExpenses));
-	}
-	
-	/**
-	 * Print Expense report header
-	 */
-	private static void printExpenseReportHeader() {
-
-		printAndFormatColumns("", "Expense Report", "");
-		printAndFormatColumns("", "--------------", "");
-		System.out.println("");
-		
-	}
-
-	/**
-	 * Calculates and prints the expense list
-	 */
-	private static void calculateAndPrintExpenseList() {
-		HashMap<String, ArrayList<TransactionNode>> expenseMap = groupTransactionsByExpense();
-		
-		// Nifty decimal formatting pattern found online!
-		DecimalFormat currencyFormat = new DecimalFormat("$#,##0.00;-$#,##0.00");
-		
-		printExpenseReportHeader();
-		
-		for (String expense : expenseMap.keySet()) {
-			System.out.println(expense);
-			System.out.println("");
-			
-			printAndFormatColumns("", "Name", "Expense");
-			printAndFormatColumns("", "----", "-------");
-			
-			double ledgeExpense = 0;
-			
-			ArrayList<TransactionNode> transactions = expenseMap.get(expense);
-			for (TransactionNode trans : transactions) {
-				printAndFormatColumns("", 
-									  trans.company.substring(0, 10), 
-									  currencyFormat.format(trans.amount));
-				
-				ledgeExpense += trans.amount;
-			}
-			
-			System.out.println("");
-			printAndFormatColumns("Total", "", currencyFormat.format(ledgeExpense));
-			System.out.println("");
-		}
-		
-	}
-	
-	/**
-	 * Helper function which changes groupings for transactions (Expenses instead of date)
-	 * It removes all payments from the final map since this only considers expenses.
-	 * @return new hashmap with the Ledger as the key
-	 */
-	private static HashMap<String, ArrayList<TransactionNode>> groupTransactionsByExpense(){
-		HashMap<String, ArrayList<TransactionNode>> newMap = new HashMap<String, ArrayList<TransactionNode>>();
-		
-		ArrayList<TransactionNode> transDate;
-		for (String date : transactionsMap.keySet()) {
-			transDate = transactionsMap.get(date);
-			
-			for (TransactionNode node : transDate) {
-				if (!node.ledger.equals("")) {
-					if (newMap.containsKey(node.ledger)) {
-						newMap.get(node.ledger).add(node);
-					} else {
-						ArrayList<TransactionNode> newList = new ArrayList<TransactionNode>();
-						newList.add(node);
-						newMap.put(node.ledger, newList);
-					}
-				}
-			}
-		}
-		return newMap;
-	}
-	
-	/**
-	 * Formats arguments passed on as evenly spaced columns
-	 * @param columns Variable argument; each column to print and format
-	 */
-	private static void printAndFormatColumns(String... columns) {
-		// Assume no string is larger than 25 characters
-		String spaces = "                         ";
-		
-		for (String col : columns) {
-			System.out.print(col);
-			if (col.length() < spaces.length())
-				System.out.print(spaces.substring(col.length()));
-		}
-		System.out.println("");
-	}
-
-	
-	/**
-	 * Calculate how many transactions are left
-	 * @param currentTotal Transactions expected
-	 * @param transOnPage Transactions on current page
-	 * @return # of transactions left [to load]
-	 */
-	private static int calculateTransactionsLeft(int currentTotal, int transOnPage) {
-		return currentTotal - transOnPage;
-	}
-	
-	
-	/**
-	 * Parses the JSON objects retrieved from the API, creates TransactionNodes for each
-	 * to store them in memory and adds them in the transactionsMap (HashMap) under appropriate day
-	 * @param pages List containing all pages loaded from API
-	 */
-	private static void parseTransactions(ArrayList<JSONObject> pages) {
-		
-		for (JSONObject page : pages) {
-			try {
-				
-				JSONArray transactions = page.getJSONArray(Constants.TRANSACTION_KEY);
-				
-				for (int i = 0; i < transactions.length(); i++) {
-					JSONObject trans = transactions.getJSONObject(i);
-					
-					String date = trans.getString(Constants.TRANSACTION_DATE_KEY);
-					String ledger = trans.getString(Constants.TRANSACTION_LEDGER_KEY);
-					String amount = trans.getString(Constants.TRANSACTION_AMOUNT_KEY);
-					String company = trans.getString(Constants.TRANSACTION_COMPANY_KEY);
-					
-					//Create new node which will convert/format the date and amount as necessary
-					TransactionNode transaction = new TransactionNode(date, ledger, amount, company);
-					
-					if(transactionsMap.containsKey(date)) {
-						// Add to hashmap
-						transactionsMap.get(date).add(transaction);
-					} else {
-						// Create new entry
-						ArrayList<TransactionNode> listOfTransactions = new ArrayList<TransactionNode>();
-						listOfTransactions.add(transaction);
-						transactionsMap.put(date, listOfTransactions);
-					}
-				}
-				
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
 	
 	/**
 	 * Connect to API and retrieve pages of transactions
@@ -449,5 +184,268 @@ public class restTest {
 		
 		return transObj;
 	}
+	
+	/**
+	 * Calculate how many transactions are left
+	 * @param currentTotal Transactions expected
+	 * @param transOnPage Transactions on current page
+	 * @return # of transactions left [to load]
+	 */
+	private static int calculateTransactionsLeft(int currentTotal, int transOnPage) {
+		return currentTotal - transOnPage;
+	}
+	
+	/**
+	 * Parses the JSON objects retrieved from the API, creates TransactionNodes for each
+	 * to store them in memory and adds them in the transactionsMap (HashMap) under appropriate day
+	 * @param pages List containing all pages loaded from API
+	 */
+	private static void parseTransactions(ArrayList<JSONObject> pages) {
+		
+		for (JSONObject page : pages) {
+			try {
+				
+				JSONArray transactions = page.getJSONArray(Constants.TRANSACTION_KEY);
+				
+				for (int i = 0; i < transactions.length(); i++) {
+					JSONObject trans = transactions.getJSONObject(i);
+					
+					String date = trans.getString(Constants.TRANSACTION_DATE_KEY);
+					String ledger = trans.getString(Constants.TRANSACTION_LEDGER_KEY);
+					String amount = trans.getString(Constants.TRANSACTION_AMOUNT_KEY);
+					String company = trans.getString(Constants.TRANSACTION_COMPANY_KEY);
+					
+					//Create new node which will convert/format the date and amount as necessary
+					TransactionNode transaction = new TransactionNode(date, ledger, amount, company);
+					
+					if(transactionsMap.containsKey(date)) {
+						// Add to hashmap
+						transactionsMap.get(date).add(transaction);
+					} else {
+						// Create new entry
+						ArrayList<TransactionNode> listOfTransactions = new ArrayList<TransactionNode>();
+						listOfTransactions.add(transaction);
+						transactionsMap.put(date, listOfTransactions);
+					}
+				}
+				
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Formats arguments passed on as evenly spaced columns
+	 * @param columns Variable argument; each column to print and format
+	 */
+	private static void printAndFormatColumns(String... columns) {
+		// Assume no string is larger than 25 characters
+		String spaces = "                         ";
+		
+		for (String col : columns) {
+			System.out.print(col);
+			if (col.length() < spaces.length())
+				System.out.print(spaces.substring(col.length()));
+		}
+		System.out.println("");
+	}
+	
+	/**
+	 * Format and print appropriate header for Daily balance report
+	 */
+	private static void printDailyBalanceHeader() {
+		if (verbose || daily || dailyVerbose) {
+			System.out.println("\n\n");
+			printAndFormatColumns("", "Daily Balance Report", "");
+			printAndFormatColumns("", "--------------------", "");
+			System.out.println("\n\n");
+		}
+		
+		if (!verbose && daily) {
+			printAndFormatColumns("Daily Balance", "Payments", "Expenses", "Date");
+			printAndFormatColumns("-------------", "--------", "--------", "----");
+		} else if (verbose || dailyVerbose) {
+			printAndFormatColumns("Company", "Daily Balance", "Payments", "Expenses", "Date");
+			printAndFormatColumns("-------", "-------------", "--------", "--------", "----");
+		}
+	}
+	
+	/**
+	 * Print Summary report headers
+	 */
+	private static void printSummaryHeader() {
+		
+		printAndFormatColumns("", "Summary Report", "");
+		printAndFormatColumns("", "--------------", "");
+		System.out.println("\n");
+		printAndFormatColumns("Final Balance", "Total Payments", "Total Expenses");
+		printAndFormatColumns("-------------", "--------------", "--------------");
+		
+	}
+	
+	/**
+	 * Print Expense report header
+	 */
+	private static void printExpenseReportHeader() {
 
+		printAndFormatColumns("", "Expense Report", "");
+		printAndFormatColumns("", "--------------", "");
+		System.out.println("");
+		
+	}
+
+	/**
+	 * Calculates daily and total balance.
+	 * It brings the daily balance only if the flag is set, otherwise 
+	 * only calculates and prints the final summary
+	 */
+	private static void calculateAndPrintBalance(){		
+		// Sort dates stored in transactionsMap
+		ArrayList<String> dates = new ArrayList<String>(transactionsMap.keySet());
+		java.util.Collections.sort(dates);
+		ArrayList<TransactionNode> transactions;
+		
+		// Nifty decimal formatting pattern found online!
+		DecimalFormat currencyFormat = new DecimalFormat("$#,##0.00;-$#,##0.00");
+		
+		// Totals
+		double totalBalance = 0;
+		double totalExpenses = 0;
+		double totalPayments = 0;
+		
+		// Daily figures
+		double dailyBalance;
+		double dailyExpenses;
+		double dailyPayments;
+		
+		printDailyBalanceHeader();
+		
+		for (String date : dates) {
+			transactions = transactionsMap.get(date);
+			
+			dailyBalance = 0;
+			dailyExpenses = 0;
+			dailyPayments = 0;
+			
+			for (TransactionNode trans : transactions) {
+				
+				dailyBalance += trans.amount;
+				
+				if (trans.amount < 0) {
+					
+					if (verbose || dailyVerbose)
+						printAndFormatColumns(trans.getFormattedCompanyName(),
+											  "",
+											  "",
+											  currencyFormat.format(trans.amount),
+											  date);
+					
+					dailyExpenses += trans.amount;
+				} else {
+					if (verbose || dailyVerbose)
+						printAndFormatColumns(trans.getFormattedCompanyName(),
+											  "",
+											  currencyFormat.format(trans.amount),
+											  "",
+											  date);
+					
+					dailyPayments += trans.amount;
+				}
+			}
+			
+			totalBalance += dailyBalance;
+			totalExpenses += dailyExpenses;
+			totalPayments += dailyPayments;
+			
+			if (!verbose && daily){
+				
+				printAndFormatColumns(currencyFormat.format(totalBalance),
+									  currencyFormat.format(dailyPayments),
+								   	  currencyFormat.format(dailyExpenses),
+								   	  date);
+				System.out.println("");
+				
+			} else if (verbose || dailyVerbose) {
+				
+				System.out.println("");
+				printAndFormatColumns("Total", 
+									  currencyFormat.format(dailyBalance),
+									  currencyFormat.format(dailyPayments),
+									  currencyFormat.format(dailyExpenses),
+									  "");
+				System.out.println("");
+				
+			}
+		}
+		
+		printSummaryHeader();
+		
+		printAndFormatColumns(currencyFormat.format(totalBalance),
+							  currencyFormat.format(totalPayments),
+							  currencyFormat.format(totalExpenses));
+	}
+
+	/**
+	 * Calculates and prints the expense list
+	 */
+	private static void calculateAndPrintExpenseList() {
+		HashMap<String, ArrayList<TransactionNode>> expenseMap = groupTransactionsByExpense();
+		
+		// Nifty decimal formatting pattern found online!
+		DecimalFormat currencyFormat = new DecimalFormat("$#,##0.00;-$#,##0.00");
+		
+		printExpenseReportHeader();
+		
+		for (String expense : expenseMap.keySet()) {
+			System.out.println(expense);
+			System.out.println("");
+			
+			printAndFormatColumns("", "Name", "Expense");
+			printAndFormatColumns("", "----", "-------");
+			
+			double ledgeExpense = 0;
+			
+			ArrayList<TransactionNode> transactions = expenseMap.get(expense);
+			for (TransactionNode trans : transactions) {
+				printAndFormatColumns("", 
+									  trans.getFormattedCompanyName(), 
+									  currencyFormat.format(trans.amount));
+				
+				ledgeExpense += trans.amount;
+			}
+			
+			System.out.println("");
+			printAndFormatColumns("Total", "", currencyFormat.format(ledgeExpense));
+			System.out.println("");
+		}
+		
+	}
+	
+	/**
+	 * Helper function which changes groupings for transactions (Expenses instead of date)
+	 * It removes all payments from the final map since this only considers expenses.
+	 * @return new hashmap with the Ledger as the key
+	 */
+	private static HashMap<String, ArrayList<TransactionNode>> groupTransactionsByExpense(){
+		HashMap<String, ArrayList<TransactionNode>> newMap = new HashMap<String, ArrayList<TransactionNode>>();
+		
+		ArrayList<TransactionNode> transDate;
+		for (String date : transactionsMap.keySet()) {
+			transDate = transactionsMap.get(date);
+			
+			for (TransactionNode node : transDate) {
+				if (!node.ledger.equals("")) {
+					if (newMap.containsKey(node.ledger)) {
+						newMap.get(node.ledger).add(node);
+					} else {
+						ArrayList<TransactionNode> newList = new ArrayList<TransactionNode>();
+						newList.add(node);
+						newMap.put(node.ledger, newList);
+					}
+				}
+			}
+		}
+		return newMap;
+	}
 }
